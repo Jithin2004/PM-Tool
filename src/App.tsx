5e8271a25a2fbd6e9f07c24157afadc95886c692
Jithin M <jithinragesh@gmail.com>
2026-05-15 11:17:53 +0530

    Fix: Increase ConfirmationModal z-index to appear above ProjectDetailsModal

--- src/App.tsx
+++ src/App.tsx
@@ -332,7 +332,7 @@ function ConfirmationModal({ isOpen, title, message, onConfirm, onCancel }: { is
 
   return (
     <AnimatePresence>
-      <div className="fixed inset-0 z-[110] flex items-center justify-center p-6">
+      <div className="fixed inset-0 z-[200] flex items-center justify-center p-6">
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
