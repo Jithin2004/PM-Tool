5a4258346beb8fa608bd2c0d293e1b4f8711bbb5
Jithin M <jithinragesh@gmail.com>
2026-05-22 18:03:00 +0530

    fix: await registerDebugTools before recovery import

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import { registerDebugTools } from './debug/registerDebugTools';
 
 export default function App() {
   useEffect(() => {
-    registerDebugTools();
-
-    import('./services/syntheticStressTest').then(m => {
-      m.recoverAbandonedStressRuns().then(r => {
-        if (r.recovered) {
-          console.log('[Stress Recovery] Abandoned synthetic runs cleaned up:', r.details);
-        }
+    registerDebugTools().then(() => {
+      import('./services/syntheticStressTest').then(m => {
+        m.recoverAbandonedStressRuns().then(r => {
+          if (r.recovered) {
+            console.log('[Stress Recovery] Abandoned synthetic runs cleaned up:', r.details);
+          }
+        });
       });
     });
   }, []);
