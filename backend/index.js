39b656a3401dd533af0d7c78b505c43b42df8940
shamil <for.photos.only001@gmail.com>
2026-05-22 22:25:21 +0530

    magic mashroom this is last time

--- backend/index.js
+++ backend/index.js
@@ -5,16 +5,9 @@ const connectDB = require('./config/db')
 const app = express()
 const PORT = process.env.PORT || 5000
 
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    return res.status(200).json({});
-  }
-  next();
-});
 
 app.get('/',(req,res)=>{
     res.status(200).json({
